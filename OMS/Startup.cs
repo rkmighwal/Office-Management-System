ea8bd4c5a6f321066b139d645f61225b175dc26e
rkmighwal <rkmighwal@gmail.com>
2015-02-20 16:48:57 +0530

    Intializing Repository & Add MVC Project

---
+++ OMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OMS.Startup))]
+namespace OMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

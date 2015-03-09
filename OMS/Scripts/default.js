3f0d77e8accf59d2a8157fffae48c52cce831384
rkmighwal <rkmighwal@gmail.com>
2015-03-09 18:35:26 +0530

    Add Modal Dialog Form Script & Update in Layout

--- OMS/Scripts/default.js
+++ OMS/Scripts/default.js
@@ -11,4 +11,51 @@ $(document).ready( function() {
 		}
 	});  // sidebar menu click
 
+    // Model Form Functions
+
+	jQuery.ajaxSetup({ cache: false });
+
+	jQuery("a[data-modal]").on("click", function (e) {
+
+	    // hide dropdown if any
+	    jQuery(e.target).closest('.btn-group').children('.dropdown-toggle').dropdown('toggle');
+
+
+	    jQuery('#modelContent').load(this.href, function () {
+	        jQuery('#modalPlaceHolder').modal({
+	            /*backdrop: 'static',*/
+	            keyboard: true
+	        }, 'show');
+
+	        bindForm(this);
+	    });
+
+	    return false;
+	});
+
 }); // document.ready
+
+function bindForm(dialog) {
+
+    jQuery('form', dialog).submit(function () {
+        jQuery.ajax({
+            url: this.action,
+            type: this.method,
+            data: jQuery(this).serialize(),
+            success: function (result) {
+                if (result.message)
+                    alert(result.message);
+
+                if (result.success) {
+                    jQuery('#modalPlaceHolder').modal('hide');
+                    //Refresh
+                    location.reload();
+                } else {
+                    jQuery('#modelContent').html(result);
+                    bindForm();
+                }
+            }
+        });
+        return false;
+    });
+}
